9f77dc531d2ca6dfe15e8082e08a33da8e986180
S-Husan <sayfiyevhusan777@gmail.com>
2026-06-25 11:26:58 +0500

    I revised topics from 1-14

---
+++ Revision.py
@@ -0,0 +1,53 @@
+# Starting the rivision 
+
+# | Slice |
+
+# a = "hello, world"
+# print(a[:4])
+# 
+# print(a[5:])
+# 
+
+# || Modify Strings ||
+
+# |Upper Case| == > upper()
+# a = "Jerry is grey"
+# print(a.upper())
+
+# |Lower Case| == > lower()
+# a = "JARRY IS FREYY"
+# print(a.lower())
+
+
+# | Remove White Space | == > strip() method
+# a = " Hello, Larry "
+# print(a)
+# print(a.strip())
+
+# | Replace String | == > Replace()
+# a = "Hello , World!"
+# print(a.replace("e","C"))
+
+# | Split String | == > split()
+# a = "Hello , World"
+# b = a.split(",")
+# print(b)
+# print(a)
+
+# | String Concatenation |
+# a = "My"
+# b = "name"
+# c = "is"
+# d = "Ali"
+# print(a +" "+ b +" "+ c +" "+ d)
+
+# | String Format | == > format()
+# age = 18
+# name= "husan"
+# txt= "Hi my name is {} and i am {} years old"
+# print(txt.format(name,age))
+
+# | Escape Character | === > " \" "
+
+txt = "We are the so-colled \"Vikings\" from the north"
+print(txt)
